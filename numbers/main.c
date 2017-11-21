e48646445f82fc440219fdc6416d009d2e18f862
Felix Wetell <felixwetell@users.noreply.github.com>
2017-11-21 13:56:55 +0100

    Initial Commit

---
+++ numbers/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  numbers
+//
+//  Created by Felix Wetell on 2017-11-21.
+//  Copyright © 2017 Felix Wetell. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
