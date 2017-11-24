ace2561b5cc6f4484a855f859f2625cc7c553465
Felix Wetell <felixwetell@users.noreply.github.com>
2017-11-24 15:15:35 +0100

    Refactored som code and added a quicksort

--- numbers/main.c
+++ numbers/main.c
@@ -20,16 +20,16 @@ int duplicate(int array[100], int number){
 // Prints the array of number in the correct format
 void print(int array[]){
     int i;
-    int rowsum=0, val = 0, col, columsum[10]={0}, row;
+    int rowsum = 0, val = 0, col, columsum[10] = {0}, row;
     for (row = 0; row < 10; row++) {
         for (col = 0; col < 10; col++) {
             printf("%i ", array[val]);
-            rowsum+=array[val];
-            columsum[col]+=array[val];
+            rowsum += array[val];
+            columsum[col] += array[val];
 
             val++;
         }
-        printf(" :: Radsumman av rad %i är %i\n",row+1,rowsum);
+        printf(" :: Radsumman av rad %i är %i\n",row + 1,rowsum);
         rowsum = 0;
     }
 
@@ -42,33 +42,38 @@ void print(int array[]){
 }
 
 // Sorts the array from lowest number to the highest
-int sort(int array[]) {
-    int i,j,q,temp;
+int * sort(int array[]) {
+    int i, j, temp;
 
     for (i = 0; i < 100 - 1; i++) {
         for (j = 0; j < 100 - 1; j++)
         {
             if (array[j]>array[j+1])
             {
-                temp=array[j];
-                array[j]=array[j+1];
-                array[j+1]=temp;
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
             }
         }
     }
     return array;
 }
 
+// Quicksort compare function
+int cmpfunc (const void * a, const void * b) {
+    return (* (int*) a - *(int*) b);
+}
+
 void count(int array[])
 {
-    int i,sum;
+    int i, sum = 0;
     for (i = 0; i < 100; i++)
     {
         sum+=array[i];
     }
     sum/=100;
-    printf("Medelvärdet är: %i\n",sum);
-    printf("Medianen är: %i\n",(array[49]+array[50])/2);
+    printf("Medelvärdet är: %i\n", sum);
+    printf("Medianen är: %i\n", (array[49] + array[50]) / 2);
     printf("Maxvärde: %i\n", array[99]);
     printf("Minvärde: %i\n", array[0]);
 }
@@ -96,7 +101,8 @@ int main(){
     // Printing unsorted array (1)
     printf("Unsorted array: \n");
     print(unsorted_numbers);
-
+    printf("\n");
+    
     // Copying the unsorted list before modifying
     for (j = 0; j < sizeof(unsorted_numbers)/sizeof(int); j++) {
         sorted_numbers[j] = unsorted_numbers[j];
@@ -105,8 +111,10 @@ int main(){
     // Sorting and printing the array (2)
     printf("Sorted array: \n");
     sort(sorted_numbers);
+    //qsort(sorted_numbers, 100, sizeof(int), cmpfunc);
     print(sorted_numbers);
-
+    printf("\n");
+    
     // Prints out Calculations (3)
     count(sorted_numbers);
 
